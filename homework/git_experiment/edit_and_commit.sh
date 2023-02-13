cd69326d6caad2ce36967acecc90dba055e0101e
Jungin Choi <jchoi177@jhu.edu>
2023-02-13 17:55:49 -0500

    edit and run edit_and_commit.sh

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -3,5 +3,8 @@
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
-  # Fill in
+  sed -i "$linenum i This is a new sentence." foo.tex
+  pdflatex foo.tex
+  git add -u
+  git commit -m "add $commit_count -th sentence"
 done
